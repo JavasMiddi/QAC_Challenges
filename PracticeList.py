0c66b56554780df0db97ae5d93a78a76859fa95e
Javas Middleton <javasmiddleton@icloud.com>
2020-07-20 20:12:08 +0100

    Updated files

---
+++ PracticeList.py
@@ -0,0 +1,5 @@
+July = []
+July.extend(('Javas', 'John', 'Jason', 'Arsalan', 'Domenico', 'Josh', 'Ryan', 'Bradley', 'Thembi', 'Jacob', 'Clifford', 'Wasim', 'Mowafak', 'Tobias', 'Chris', 'Sam', 'Ed', 'Amanda', 'Steven', 'Jack', 'Chris'))
+July.append('Luke')
+print(July[4])
+print(July.count('Chris'))
