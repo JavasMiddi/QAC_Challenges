7ffd20dc3961099d23be4a63af09f431f05cb293
Javas Middleton <javasmiddleton@icloud.com>
2020-07-27 18:10:55 +0100

    update

--- seven_not_five.py
+++ seven_not_five.py
@@ -1,13 +1,8 @@
 import pytest
-from random import randint
 def snotf():
-    list1 = []
+    final = []
     for x in range(2000, 3201):
         if x % 7 == 0:
             if x % 5 != 0:
                 list1.append(x)
-    
-    return str(list1).strip('[]')
-    
-def test_seven():
-    pass
+    return str(final).strip('[]')
---
+++ test_seven_not_five.py
@@ -0,0 +1,10 @@
+import pytest, random
+from seven_not_five import snotf
+def test_seven():
+    '''x = (random.choice(snotf))
+    assert x % 7 == 0'''
+    pass
+def test_five():
+    '''y = (random.choice(snotf))
+    assert y % 5 != 0'''
+    pass
